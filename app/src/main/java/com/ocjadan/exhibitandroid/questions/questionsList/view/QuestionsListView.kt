6878f2e1123bbb257c427e3f509a24807f8fcb82
cheou <chengjie.ou@publicissapient.com>
2022-03-15 12:08:50 -0700

    ui clean up

--- app/src/main/java/com/ocjadan/exhibitandroid/questions/questionsList/view/QuestionsListView.kt
+++ app/src/main/java/com/ocjadan/exhibitandroid/questions/questionsList/view/QuestionsListView.kt
@@ -1,46 +1,23 @@
 package com.ocjadan.exhibitandroid.questions.questionsList.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.ocjadan.exhibitandroid.R
-import com.ocjadan.exhibitandroid.common.ui.Toolbar
 import com.ocjadan.exhibitandroid.questions.Question
 import com.ocjadan.exhibitandroid.questions.QuestionView
-import com.ocjadan.exhibitandroid.common.ui.theme.ExhibitAndroidTheme
 
 @Composable
 internal fun QuestionsListView(questions: List<Question>, onQuestionClicked: (question: Question) -> Unit) {
-    ExhibitAndroidTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-            Column {
-                Toolbar(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp, 12.dp),
-                    startIcon = R.drawable.ic_baseline_account_circle_24,
-                    startIconDescription = R.string.toolbar_account_button,
-                    title = R.string.toolbar_app_title) {
-                }
-                if (questions.isEmpty()) {
-                    Text("No questions")
-                } else {
-                    LazyColumn {
-                        items(questions) {
-                            QuestionView(Modifier.padding(0.dp, 0.dp, 0.dp, 16.dp), it, onQuestionClicked)
-                        }
-                    }
-                }
+    if (questions.isEmpty()) {
+        Text("No questions")
+    } else {
+        LazyColumn {
+            items(questions) {
+                QuestionView(Modifier.padding(0.dp, 0.dp, 0.dp, 16.dp), it, onQuestionClicked)
             }
         }
     }
--- app/src/main/java/com/ocjadan/exhibitandroid/questions/questionsList/view/QuestionsListViewController.kt
+++ app/src/main/java/com/ocjadan/exhibitandroid/questions/questionsList/view/QuestionsListViewController.kt
@@ -2,8 +2,18 @@ package com.ocjadan.exhibitandroid.questions.questionsList.view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import com.ocjadan.exhibitandroid.R
+import com.ocjadan.exhibitandroid.common.ui.Toolbar
+import com.ocjadan.exhibitandroid.common.ui.theme.ExhibitAndroidTheme
 import com.ocjadan.exhibitandroid.common.viewcontroller.BaseObservableViewController
 import com.ocjadan.exhibitandroid.questions.Question
 
@@ -19,7 +29,22 @@ class QuestionsListViewController(layoutInflater: LayoutInflater, viewGroup: Vie
 
     private fun compose() {
         (getRootView() as ComposeView).setContent {
-            QuestionsListView(questions, ::onQuestionClicked)
+            ExhibitAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Column {
+                        Toolbar(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp, 12.dp),
+                            startIcon = R.drawable.ic_baseline_account_circle_24,
+                            startIconDescription = R.string.toolbar_account_button,
+                            title = R.string.toolbar_app_title
+                        )
+                        QuestionsListView(questions, ::onQuestionClicked)
+                    }
+                }
+            }
         }
     }
 
